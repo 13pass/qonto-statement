cbbf61ef5429270683de39f68656cae4b7fcf30d
vincent HETRU <vincent.hetru@gmail.com>
2021-03-15 14:39:31 +0000

    use new iban

--- cronJob.js
+++ cronJob.js
@@ -6,23 +6,28 @@ const moment = require('moment');
 require('dotenv').config();
 const accountStatement = require('./accountStatement');
 const dropbox = require('./dropbox');
+
+async function generateMonthReport({iban, nbMonthBefore = 0}) {
+  let previousMonth = new moment().subtract(1 + nbMonthBefore, 'months');
+  let dropboxPath = process.env.DROPBOX_FOLDER + previousMonth.format('YYYY') + '/relevés/'; 
+  let filePath = dropboxPath + previousMonth.format('YYYY-MM') + '-qonto-' + iban + '.csv';
+  let lastMonth = await accountStatement.getLastMonthStatement(iban);
+  for (transaction of lastMonth.transactions) {
+    transaction.date = transaction.settled_at.slice(0,7);
+  }
+  let groupedTransactions = _.groupBy(lastMonth.transactions, 'date');
+  let previousMonthTransactions = groupedTransactions[previousMonth.format('YYYY-MM')];
+  if (previousMonthTransactions) {
+    let exportCsv = await accountStatement.exportToCsv(previousMonthTransactions);
+    await dropbox.upload(process.env.DROPBOX_TOKEN, filePath, exportCsv);
+  }
+}
+
 (async() => {
   try {
     let ibans = process.env.IBANS.split(',');
-    let previousMonth = new moment().subtract(1, 'months');
-    let dropboxPath = process.env.DROPBOX_FOLDER + previousMonth.format('YYYY') + '/relevés/'; 
     if (ibans.length) {
-      ibans = await Promise.all(ibans.map(async iban => {
-        let filePath = dropboxPath + previousMonth.format('YYYY-MM') + '-qonto-' + iban + '.csv';
-        let lastMonth = await accountStatement.getLastMonthStatement(iban);
-        for (transaction of lastMonth.transactions) {
-          transaction.date = transaction.settled_at.slice(0,7);
-        }
-        let groupedTransactions = _.groupBy(lastMonth.transactions, 'date');
-        let previousMonthTransactions = groupedTransactions[previousMonth.format('YYYY-MM')];
-        let exportCsv = await accountStatement.exportToCsv(previousMonthTransactions);
-        await dropbox.upload(process.env.DROPBOX_TOKEN, filePath, exportCsv);
-      }));
+      await generateMonthReport({iban: ibans[0]});
     }
   } catch (error) {
     console.error(error);   
